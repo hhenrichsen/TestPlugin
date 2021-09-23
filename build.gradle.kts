464b720d1cd7b9ff708581ecee37b0ce2a7163f3
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2021-09-23 02:21:52 -0600

    Add test coverage report to the check task.

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,6 @@ repositories {
 dependencies {
     // We want Spigot
     implementation("org.spigotmc:spigot-api:1.17-R0.1-SNAPSHOT")
-    implementation("org.junit.jupiter:junit-jupiter:5.7.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.0")
@@ -39,3 +38,5 @@ tasks.withType<JacocoReport> {
         html.required.set(false)
     }
 }
+
+tasks.getByName("check").dependsOn(tasks.getByName("jacocoTestReport"))
